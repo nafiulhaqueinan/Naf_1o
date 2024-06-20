5e4daac0fc22b2811f09413cd29f9571cce4545f
Md.Nafiul Haque <54739121+nafiulhaqueinan@users.noreply.github.com>
2024-06-20 14:36:53 +0600

    Update hashkey.cpp

--- hashkey.cpp
+++ hashkey.cpp
@@ -1,28 +1,32 @@
 //
 // Created by Md.NAFIUL HAQUE on 5/27/2024.
 //
+
 #include "defination.h"
+#include <cassert>
 
-ll64 generatePositionKey( const S_BOARD *pos){
-    int sq=0;
-    ll64 finalkey=0;
-    int piece = EMPTY;
-    for( sq=0 ; sq<Board_sq_Num ; sq++){
-       piece=pos->pieces[sq];
-       if(piece != NO_SQ && piece != EMPTY){
-           assert(piece>=wP && piece<=bK);
-           finalkey ^= piecekeys[piece][sq];
-       }
+uint64_t generatePositionKey(const S_BOARD &pos) {
+    uint64_t finalKey = 0;
+
+    for (int sq = 0; sq < Board_sq_Num; ++sq) {
+        int piece = pos.pieces[sq];
+        if (piece != NO_SQ && piece != EMPTY) {
+            assert(piece >= wP && piece <= bK);
+            finalKey ^= piecekeys[piece][sq];
+        }
     }
-    if(pos->side == WHITE){
-        finalkey ^= sideKey;
+
+    if (pos.side == WHITE) {
+        finalKey ^= sideKey;
     }
-    if(pos->enPas != NO_SQ){
-        assert(pos->enPas>=0 && pos->enPas<Board_sq_Num);
-        finalkey ^= piecekeys[EMPTY][pos->enPas];
+
+    if (pos.enPas != NO_SQ) {
+        assert(pos.enPas >= 0 && pos.enPas < Board_sq_Num);
+        finalKey ^= piecekeys[EMPTY][pos.enPas];
     }
-    assert(pos->castleperm>=0 && pos->castleperm<=15);
-    finalkey ^= Castlekeys[pos->castleperm];
-    return finalkey;
+
+    assert(pos.castleperm >= 0 && pos.castleperm <= 15);
+    finalKey ^= Castlekeys[pos.castleperm];
+
+    return finalKey;
 }
-

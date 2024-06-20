a7e7233ceb74c179a8bca9c9d4dcd730fd6b95d9
Md.Nafiul Haque <54739121+nafiulhaqueinan@users.noreply.github.com>
2024-06-20 14:38:02 +0600

    Update bitboard.cpp

--- bitboard.cpp
+++ bitboard.cpp
@@ -1,50 +1,48 @@
 //
 // Created by Md.NAFIUL HAQUE on 5/24/20
-#include "bits/stdc++.h"
+//
+
+#include <bits/stdc++.h>
 #include "defination.h"
 using namespace std;
 
-
 const int BitTable[64] = {
-        63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
-        51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52,
-        26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28,
-        58, 20, 37, 17, 36, 8
+    63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
+    51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52,
+    26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28,
+    58, 20, 37, 17, 36, 8
 };
 
-int PopBit(ll64 *bb) {
-    ll64 b = *bb ^ (*bb - 1);
-    unsigned int fold = (unsigned) ((b & 0xffffffff) ^ (b >> 32));
-    *bb &= (*bb - 1);
+int PopBit(uint64_t &bb) {
+    uint64_t b = bb ^ (bb - 1);
+    unsigned int fold = static_cast<unsigned int>((b & 0xffffffff) ^ (b >> 32));
+    bb &= (bb - 1);
     return BitTable[(fold * 0x783a9b23) >> 26];
 }
 
-int CountBits(ll64 b) {
-    int r;
-    for(r = 0; b; r++, b &= b - 1);
+int CountBits(uint64_t b) {
+    int r = 0;
+    while (b) {
+        r++;
+        b &= b - 1;
+    }
     return r;
 }
 
-void PrintBitBoard(ll64 bb){
-    ll64 shiftMe= 1ULL;
-    int rank =0;
-    int file=0;
-    int sq=0;
-    int sq64 =0;
+void PrintBitBoard(uint64_t bb) {
+    uint64_t shiftMe = 1ULL;
 
-    cout<<"\n";
-    for(rank = RANK_8;rank>=RANK_1;rank--){
-        for(file = FILE_A; file<=FILE_H;file++){
-            sq= FR2SQ(file,rank);
-            sq64= SQ64(sq);
-            if((shiftMe<<sq64)& bb)
-                cout<<"X";
+    cout << "\n";
+    for (int rank = RANK_8; rank >= RANK_1; rank--) {
+        for (int file = FILE_A; file <= FILE_H; file++) {
+            int sq = FR2SQ(file, rank);
+            int sq64 = SQ64(sq);
+            if ((shiftMe << sq64) & bb)
+                cout << "X";
             else
-                cout<<"_";
+                cout << "_";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-    cout<<"\n";
-
-
+    cout << "\n";
 }

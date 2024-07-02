cc6f88db82f92d74d7adc7f94ddad322744d9c63
Md. Nafiul Haque <54739121+nafiulhaqueinan@users.noreply.github.com>
2024-07-02 23:42:43 +0600

    Update bitboard.cpp

--- bitboard.cpp
+++ bitboard.cpp
@@ -6,7 +6,7 @@
 #include <bits/stdc++.h>
 #include "defination.h"
 using namespace std;
-
+//here is the board structure
 const int BitTable[64] = {
     63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
     51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52,
